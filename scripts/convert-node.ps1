106953f2179e4b09f0373504a4dc4772e326d98d
elmore schamberger <elmore-schamberger@outlook.com>
2025-07-30 18:32:33 +0330

    Update convert-node.ps1

--- scripts/convert-node.ps1
+++ scripts/convert-node.ps1
@@ -1,80 +1,82 @@
-# Install required modules
-Install-PackageProvider -Name NuGet -Force
-Install-Module -Name Selenium -Force
+# Ensure TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Import modules
+# Install NuGet provider
+if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue -ListAvailable)) {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ForceBootstrap | Out-Null
+}
+
+# Install Selenium module
+if (-not (Get-Module -ListAvailable -Name Selenium)) {
+    Install-Module -Name Selenium -Force -SkipPublisherCheck -AllowClobber
+}
+
 Import-Module Selenium
 
-# Download Chrome WebDriver
-$driverPath = "C:\WebDriver"
-New-Item -Path $driverPath -ItemType Directory -Force
-Invoke-WebRequest "https://chromedriver.storage.googleapis.com/LATEST_RELEASE" -OutFile "$driverPath\version.txt"
-$version = Get-Content "$driverPath\version.txt"
-Invoke-WebRequest "https://chromedriver.storage.googleapis.com/$version/chromedriver_win32.zip" -OutFile "$driverPath\chromedriver.zip"
-Expand-Archive -Path "$driverPath\chromedriver.zip" -DestinationPath $driverPath -Force
-
-# Start Chrome
+# Start Chrome in headless mode
 $chromeOptions = New-Object OpenQA.Selenium.Chrome.ChromeOptions
 $chromeOptions.AddArgument("--headless")
-$driver = New-Object OpenQA.Selenium.Chrome.ChromeDriver($driverPath, $chromeOptions)
+$chromeOptions.AddArgument("--disable-gpu")
+$chromeOptions.AddArgument("--no-sandbox")
+$chromeOptions.AddArgument("--window-size=1920,1080")
 
-# Main automation script
+$driver = New-Object OpenQA.Selenium.Chrome.ChromeDriver($chromeOptions)
+
 try {
     # Open website
     $driver.Navigate().GoToUrl("https://v2rayse.com/node-convert")
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 10
 
     # Click first button
-    $button1 = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='__nuxt']/div/div/main/div/div/div/div[3]/div/div[2]/button/span[3]/i"))
+    $button1 = $driver.FindElement([OpenQA.Selenium.By]::XPath("//button[.//i[contains(@class, 'mdi-content-copy')]]"))
     $button1.Click()
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
 
     # Click second button
-    $button2 = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='v-menu-125']/div/div/div[3]/div[3]/div[2]/div/button/span[3]/i"))
+    $button2 = $driver.FindElement([OpenQA.Selenium.By]::XPath("//button[.//i[contains(@class, 'mdi-dots-vertical')]]"))
     $button2.Click()
-    Start-Sleep -Seconds 2
+    Start-Sleep -Seconds 3
 
     # Select middle option
-    $option = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='v-menu-125']/div/div/div[3]/div[2]/div[2]/div/div/div[1]/div/div[3]/div/div/span/font/font/font"))
+    $option = $driver.FindElement([OpenQA.Selenium.By]::XPath("//div[contains(text(), 'VLESS')]"))
     $option.Click()
     Start-Sleep -Seconds 2
 
     # Click confirm button
-    $confirmBtn = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='v-menu-125']/div/div/div[3]/div[2]/div[2]/div/div/div[2]/button/span[3]/i"))
+    $confirmBtn = $driver.FindElement([OpenQA.Selenium.By]::XPath("//button[.//i[contains(@class, 'mdi-check')]]"))
     $confirmBtn.Click()
     Start-Sleep -Seconds 2
 
     # Enter URL
-    $inputField = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='VVwmHIqu2LR0babO']"))
+    $inputField = $driver.FindElement([OpenQA.Selenium.By]::XPath("//input"))
     $inputField.SendKeys("https://raw.githubusercontent.com/e-schamberger/free/refs/heads/main/config/vless.json")
-    Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 2
 
     # Click conversion buttons
-    $convertBtn1 = $driver.FindElement([OpenQA.Selenium.By]::XPath("/html/body/div[3]/div[2]/div[2]/div/div[3]/div/div[2]/button/span[3]/font/font/font"))
+    $convertBtn1 = $driver.FindElement([OpenQA.Selenium.By]::XPath("//button[.//i[contains(@class, 'mdi-content-copy')]]"))
     $convertBtn1.Click()
-    Start-Sleep -Seconds 3
+    Start-Sleep -Seconds 5
 
-    $convertBtn2 = $driver.FindElement([OpenQA.Selenium.By]::XPath("/html/body/div[3]/div[2]/div[2]/div/div[2]/div/div[2]/button[2]/span[3]/i"))
+    $convertBtn2 = $driver.FindElement([OpenQA.Selenium.By]::XPath("(//button[.//i[contains(@class, 'mdi-arrow-right-bold-hexagon-outline')]])[2]"))
     $convertBtn2.Click()
-    Start-Sleep -Seconds 3
+    Start-Sleep -Seconds 5
 
     # Select third option
-    $thirdOption = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='__nuxt']/div/div/main/div/div/div/div[3]/div/div[1]/div/div[1]/div/div[3]/div/div/span"))
+    $thirdOption = $driver.FindElement([OpenQA.Selenium.By]::XPath("(//div[@class='v-list-item__title'])[3]"))
     $thirdOption.Click()
     Start-Sleep -Seconds 2
 
     # Generate result
-    $generateBtn = $driver.FindElement([OpenQA.Selenium.By]::XPath("//*[@id='__nuxt']/div/div/main/div/div/div/div[3]/div/div[1]/div/div[2]/div/div/button/span[4]/font/font/font"))
+    $generateBtn = $driver.FindElement([OpenQA.Selenium.By]::XPath("//button[.//i[contains(@class, 'mdi-download')]]"))
     $generateBtn.Click()
     Start-Sleep -Seconds 5
 
     # Get result text
-    $resultModal = $driver.FindElement([OpenQA.Selenium.By]::XPath("/html/body/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div/div/textarea"))
+    $resultModal = $driver.FindElement([OpenQA.Selenium.By]::TagName("textarea"))
     $resultText = $resultModal.GetAttribute("value")
 
     # Save to file
-    $resultText | Out-File -FilePath ".\result.txt" -Encoding utf8
-
+    $resultText | Out-File -FilePath "converted-node.txt" -Encoding utf8
 }
 finally {
     $driver.Quit()
